0b25f9ea53db15f02197e193c26ec0f9f0554643
appala venkata avinash <aa760@snu.edu.in>
2022-07-25 12:34:02 +0530

    Styled CreateMintForm component

--- client/components/CreateMintForm.jsx
+++ client/components/CreateMintForm.jsx
@@ -36,11 +36,11 @@ function CreateMintForm({ onAdd }) {
   return (
     <>
       <div className="m-14 select-none">
-        <h1 className="text-center font-bold mb-4">Fill Warranty Details</h1>
+        <h1 className="text-center font-bold font-mono mb-4">Fill Warranty Details</h1>
         <form className="">
           <div className="w-3/4 mx-auto max-w-full">
             <div className="grid sm:grid-cols-1 grid-cols-1 gap-y-2">
-              <div className="grid grid-cols-2"><div className="grid grid-cols-1">
+              {/* <div className="grid grid-cols-2"><div className="grid grid-cols-1"> */}
               <label htmlFor="itemNumber" className="mx-auto">Item Number</label>
               <input
                 id="itemNumber"
@@ -49,10 +49,11 @@ function CreateMintForm({ onAdd }) {
                 value={form.itemNumber}
                 placeholder="Item Number"
                 onChange={handleChange}
-                className="sm:w-1/2 xs:w-72 2xs:w-60 w-full border border-gray-300 rounded-lg mx-auto"
+                // className="sm:w-1/2 xs:w-72 2xs:w-60 w-full border border-gray-300 rounded-lg mx-auto"
+                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-5/6 p-2.5 bg-slate-200"
               />
-           </div>
-        <div className="grid grid-cols-1">
+           {/* </div> */}
+        {/* <div className="grid grid-cols-1"> */}
               <label htmlFor="url" className="mx-auto">URL</label>
               <input
                 type="url"
@@ -61,21 +62,24 @@ function CreateMintForm({ onAdd }) {
                 value={form.url}
                 placeholder="URL"
                 onChange={handleChange}
-                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 bg-slate-200"
+                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-5/6 p-2.5 bg-slate-200"
               />
-           </div></div>
-          
-              <label htmlfor="unlimitedTransfers">Unlimited Transfers?</label>
+           {/* </div></div> */}
+           <div className="grid gap-1 grid-cols-3 " >
+            <div className="grid  grid-cols-1">
+              <label htmlfor="unlimitedTransfers"  className="mx-auto" >Unlimited Transfers?</label>
               <select
                 id="unlimitedTransfers"
                 name="unlimitedTransfers"
                 onChange={handleChange}
+                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/4 p-2.5 bg-slate-200"
               >
                 <option value="true">True</option>
                 <option value="false">False</option>
               </select>
-           
-              <label htmlFor="transfers">Transfers</label>
+              </div>
+           <div className="grid grid-cols-1">
+              <label htmlFor="transfers"  className="mx-auto">Transfers</label>
               <input
                 type="number"
                 name="transfers"
@@ -83,9 +87,11 @@ function CreateMintForm({ onAdd }) {
                 value={form.transfers}
                 placeholder="Number of Transfers"
                 onChange={handleChange}
+                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-4/6 p-2.5 bg-slate-200"
               />
-           
-              <label htmlfor="period">Period</label>
+              </div>
+              <div className="grid grid-cols-1">
+              <label htmlfor="period" className="mx-auto">Period</label>
               <input
                 type="number"
                 name="period"
@@ -93,12 +99,17 @@ function CreateMintForm({ onAdd }) {
                 value={form.period}
                 placeholder="Period"
                 onChange={handleChange}
+                className="border mx-auto text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-50 p-2.5 bg-slate-200"
               />
+              </div>
            
+           </div>
+           </div>
+             
           </div>
-          </div>
-          <div className="flex justify-center p-4">
-          <button type="submit" onClick={addForm}>Add</button>
+
+          <div className="flex justify-center py-3 ">
+          <button className = "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" type="submit" onClick={addForm}>Add</button>
           </div>
         </form>
       </div>
