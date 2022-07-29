8be5204531f1ec69728b98aa66c6f4233c555faa
ksgr5566 <ksgr5566@gmail.com>
2022-07-29 17:36:57 +0530

    Modified next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  }
 }
 
 module.exports = nextConfig
